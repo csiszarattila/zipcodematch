3de98bfbe448bb53d89dd278b57354d5c7924813
Attila Csiszár <csiszar.ati@gmail.com>
2009-01-05 16:04:49 +0100

    Simplified and fixed migration generation

--- generators/zipcodes/zipcodes_generator.rb
+++ generators/zipcodes/zipcodes_generator.rb
@@ -1,15 +1,9 @@
 class ZipcodesGenerator < Rails::Generator::NamedBase
 	def manifest
 		assigns = {}
-		assigns[:migration_action] = "add"
-		assigns[:class_name] = "create_zipcodes"
-		assigns[:table_name] = "zipcodes"
-		assigns[:attributes] = [] 
-		assigns[:attributes] << Rails::Generator::GeneratedAttribute.new('zipcode','integer')
-		assigns[:attributes] << Rails::Generator::GeneratedAttribute.new('city','string')
 		
 		record do |m|
-			m.migration_template 'migration:migration.rb', "db/migrate", {
+			m.migration_template 'migration.rb', "db/migrate", {
 				:assigns => assigns,
 				:migration_file_name => "create_zipcodes"}
 		end
